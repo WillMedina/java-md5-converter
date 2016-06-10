9f4ef5fcd434a77c9cdaa80f723486847f576fa0
will <will@dev>
2016-06-09 23:50:07 -0500

    Initial Commit.

---
+++ src/md5.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author will
+ */
+import gui.*;
+import javax.swing.UIManager;
+
+public class md5 {
+
+    public static void main(String[] args) throws Exception {        
+        gui1 g = new gui1();
+        g.setTitle("Conversor a MD5");
+        g.setLocationRelativeTo(null);
+        g.setVisible(true);
+    }
+}
